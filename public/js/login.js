b4042de6e8619f6674647e9b7ee5ea6a75df109f
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-11-11 16:04:40 -0600

    fixed login bug, but need to verify if I'm logged in

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const logInFormHandler = async (event) => {
   const password = document.querySelector('#login-password').value.trim();
 
   if (username && password) {
-    const response = await fetch('/login', {
+    const response = await fetch('/api/user/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json'},
