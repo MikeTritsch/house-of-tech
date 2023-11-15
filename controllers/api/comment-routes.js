5ae93f8e9ad8ab67371a9b99aa078889089f3165
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-11-14 23:15:30 -0600

    working on comment functionality

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  const body = req.body;
+  console.log(req.body);
+
+  try {
+    const newComment = await Comment.create({
+      ...body,
+      user_id: req.session.user_id,
+    });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ public/js/comment.js
@@ -0,0 +1,20 @@
+const addCommentHandler = async function(event) {
+  event.preventDefault();
+
+  const post_id = parseInt(window.location.pathname.split('/').pop());
+  const comment_body = document.querySelector('.newComment').value;
+
+  if (comment_body) {
+    const response = await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({comment: comment_body, post_id }),
+      headers: { 'Content-Type': 'application/json'},
+  });
+
+  if(response.ok){
+    document.location.reload();
+  }
+}
+};
+
+document.querySelector('.commentForm').addEventListener('submit', addCommentHandler)
