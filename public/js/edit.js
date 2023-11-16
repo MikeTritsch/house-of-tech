0c8b747002e4085c5e728da2899c9b31f90450c1
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-11-15 22:01:53 -0600

    added link to edit page

---
+++ public/js/edit.js
@@ -0,0 +1,32 @@
+const editPostHandler = async function(event) {
+  event.preventDefault();
+
+  const post_id = window.location.pathname.split('/').pop();
+  const topic = document.querySelector('.newPostTitle').value;
+  const post_body = document.querySelector('.newBody').value;
+
+  const response = await fetch(`/api/post/${post_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      topic,
+      post_body,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  console.log(response);
+  
+  if (response.ok) {
+    document.location.replace('/dashboard');
+    console.log('Post successfully updated!')
+  } else {
+    console.error('Error during fetch:', error);
+    alert('Failed to update your post');
+  }
+
+  document.location.replace('/dashboard');
+};
+
+document.querySelector('.save-edit-btn').addEventListener('click', editPostHandler);
